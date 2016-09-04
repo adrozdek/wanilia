d1ead123d5564f4a501d39deed2c309aa619c657
Agata <Agata.Drozdek@dreamlab.pl>
2016-09-04 17:37:37 +0200

    Adding google maps and contact info

--- js/main.js
+++ js/main.js
@@ -1,91 +1,81 @@
-var about = document.getElementById("about");
-var menu = document.getElementById("menu");
-var info = document.getElementById("info");
-var contact = document.getElementById("contact");
-container = document.getElementById("container");
-nav = document.getElementsByClassName("page-nav")[0];
-title = document.getElementsByClassName("cafe-title")[0];
+$(document).ready(function () {
 
-about.addEventListener("click", function (e) {
-    e.preventDefault();
+    var about = document.getElementById("about");
+    var menu = document.getElementById("menu");
+    var info = document.getElementById("info");
+    var contact = document.getElementById("contact");
+    container = document.getElementById("container");
+    nav = document.getElementsByClassName("page-nav")[0];
+    title = document.getElementsByClassName("cafe-title")[0];
 
-    addHiddenClassToContainerChildren();
-    removeActiveClassFromNav();
-    this.classList.add("active");
-    title.innerHTML = "O Wanilia Cafe";
-    document.getElementsByClassName("about")[0].classList.remove("hidden");
-});
+    about.addEventListener("click", function (e) {
+        e.preventDefault();
 
-menu.addEventListener("click", function (e) {
-    e.preventDefault();
+        addHiddenClassToContainerChildren();
+        removeActiveClassFromNav();
+        this.classList.add("active");
+        title.innerHTML = "O Wanilia Cafe";
+        document.getElementsByClassName("about")[0].classList.remove("hidden");
+    });
 
-    addHiddenClassToContainerChildren();
-    removeActiveClassFromNav();
-    this.classList.add("active");
-    title.innerHTML = "Menu";
+    menu.addEventListener("click", function (e) {
+        e.preventDefault();
 
-    document.getElementsByClassName("menu")[0].classList.remove("hidden");
-});
+        addHiddenClassToContainerChildren();
+        removeActiveClassFromNav();
+        this.classList.add("active");
+        title.innerHTML = "Menu";
 
-info.addEventListener("click", function (e) {
-    e.preventDefault();
+        document.getElementsByClassName("menu")[0].classList.remove("hidden");
+    });
 
-    addHiddenClassToContainerChildren();
-    removeActiveClassFromNav();
-    this.classList.add("active");
-    title.innerHTML = "Informacje";
+    info.addEventListener("click", function (e) {
+        e.preventDefault();
 
-    document.getElementsByClassName("info")[0].classList.remove("hidden");
-});
+        addHiddenClassToContainerChildren();
+        removeActiveClassFromNav();
+        this.classList.add("active");
+        title.innerHTML = "Aktualności";
 
-contact.addEventListener("click", function (e) {
-    e.preventDefault();
+        document.getElementsByClassName("info")[0].classList.remove("hidden");
+    });
 
-    addHiddenClassToContainerChildren();
-    removeActiveClassFromNav();
-    this.classList.add("active");
-    title.innerHTML = "Kontakt";
+    contact.addEventListener("click", function (e) {
+        e.preventDefault();
 
-    document.getElementsByClassName("contact")[0].classList.remove("hidden");
-});
+        addHiddenClassToContainerChildren();
+        removeActiveClassFromNav();
+        this.classList.add("active");
+        title.innerHTML = "Informacje";
 
-counter = 0;
+        document.getElementsByClassName("contact")[0].classList.remove("hidden");
+    });
 
-$(".info .previous-info .image-wrapper img").on("click", function () {
-    console.log("ba");
-    var info_wrapper = $(".info > .image-wrapper img");
-    var info_wrapper_src = $(info_wrapper).attr("src");
-    console.log(info_wrapper_src);
+    $(".info .previous-info .image-wrapper img").on("click", function () {
+        console.log("ba");
+        var info_wrapper = $(".info > .image-wrapper img");
+        var info_wrapper_src = $(info_wrapper).attr("src");
+        console.log(info_wrapper_src);
 
-    $(info_wrapper).attr("src", $(this).attr("src"));
-    // var to_copy = $(this).outerHTML.copy().find("img").attr("src", info_wrapper_src);
+        $(info_wrapper).attr("src", $(this).attr("src"));
 
-    // $(this).hide();
-    // var to_copy = image_wrapper.find("img").attr("src", info_wrapper_src);
-    // console.log(image_wrapper);
-    // console.log(image_wrapper.find("img"));
-    // console.log(to_copy);
-    // console.log($(this).outerHTML().find("img"));
+        var x = $(".info .previous-info .image-wrapper").first();
+        console.log(x);
 
-    // $(".info .previous-info").prepend(to_copy);
-    // $(".info .previous-info").prepend("<div class='image-wrapper'></div>");
-    var x = $(".info .previous-info .image-wrapper").first();
-    console.log(x);
+        if (info_wrapper_src == "images/info1.jpg" && x.find("img").attr("src") != "images/info1.jpg") {
+            x.clone(true).removeAttr('style').find("img").attr("src", info_wrapper_src).parent().prependTo($(".info .previous-info"));
+        }
+    });
 
-    if (info_wrapper_src == "images/info1.jpg" && x.find("img").attr("src") != "images/info1.jpg") {
-        x.clone(true).removeAttr('style').find("img").attr("src", info_wrapper_src).parent().prependTo($(".info .previous-info"));
+    function addHiddenClassToContainerChildren() {
+        for (var i = 0; i < container.children.length; i++) {
+            container.children[i].classList.add("hidden");
+        }
+    }
+
+    function removeActiveClassFromNav() {
+        for (var i = 0; i < nav.children.length; i++) {
+            nav.children[i].firstChild.classList.remove("active");
+        }
     }
 });
-
-function addHiddenClassToContainerChildren() {
-    for (var i = 0; i < container.children.length; i++) {
-        container.children[i].classList.add("hidden");
-    }
-}
-
-function removeActiveClassFromNav() {
-    for (var i = 0; i < nav.children.length; i++) {
-        nav.children[i].firstChild.classList.remove("active");
-    }
-}
-
